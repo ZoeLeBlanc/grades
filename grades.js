6b86a5a49f38f90a621b056430a2f8afac552aed
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-07-20 19:07:33 -0500

    started

---
+++ grades.js
@@ -0,0 +1,18 @@
+var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+
+var gradeA = '';
+var gradeB = '';
+for (var i = 0; i < scores.length; i++){
+	if (scores[i] > 91){
+		gradeA = console.log("There are" + i + " of students with grade A.")
+	}
+} else if (scores[i] > 81){
+		gradeB = console.log(i)
+} else if (scores[i] > ){
+
+}
+}
+
+
+
+
