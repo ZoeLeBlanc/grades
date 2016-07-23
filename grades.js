896bd42706cedcd94160481216a0c158b1f488fe
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-07-23 12:52:05 -0500

    updates

--- grades.js
+++ grades.js
@@ -1,26 +1,15 @@
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+function range(upper, lower){
+	return upper > && < lower;
+}
 
-var gradeA = scores.filter(function(score) {
-	return score > 91;
-}).length;
-console.log("There are " + gradeA + " of students with a grade 91 or higher.");
+var gradeA = scores.range(function(101, 91){
+console.log(gradeA.length);
+});
 
-var gradeB = scores.filter(function(score) {
-	return score > 81 ;
-}).length;
-gradeB = gradeB - gradeA; 
-console.log("There are " + gradeB + " of students with a grade between 81 and 90.");
-
-var gradeC = scores.filter(function(score) {
-	return score > 71 ;
-}).length;
-
-gradeC = gradeC - gradeB; 
-console.log("There are " + gradeC + " of students with a grade between 71 and 80.");
-
-var gradeD = scores.filter(function(score) {
-	return score > 61 ;
-}).length;
-
-gradeD = gradeD - gradeC; 
-console.log("There are " + gradeD + " of students with a grade between 61 and 70.");
+var sortedScores = scores.sort(function (first, second){
+	return first - second;
+});
+console.log("scores", sortedScores);
+console.log("The lowest grade is" + sortedScores[0]);
+console.log("The highest grade is" )
