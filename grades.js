fd78a131e1e56a5303f240b82e2986f51ade6639
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-07-23 13:56:58 -0500

    finished

--- grades.js
+++ grades.js
@@ -1,49 +1,41 @@
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
+var gradeA = 0;
+var gradeB = 0;
+var gradeC = 0;
+var gradeD = 0;
+var gradeF = 0;
+
+for (var i = 0; i < scores.length; i++ ){
+	var score = scores[i];
+switch (true) {
+	case score < 61:
+	gradeF++
+	break;
+	case score < 71:
+	gradeD++
+	break;
+	case score < 81:
+	gradeC++
+	break;
+	case score < 91:
+	gradeB++
+	break;
+	default:
+	gradeA++
+}
+}
+console.log("There are " + gradeA + " of students with grade A." );
+console.log("There are " + gradeB + " of students with grade B." );
+console.log("There are " + gradeC + " of students with grade C." );
+console.log("There are " + gradeD + " of students with grade D." );
+console.log("There are " + gradeF + " of students with grade F." );
 
 var sortedScores = scores.sort(function (first, second){
 	return first - second;
 });
-console.log("scores", sortedScores);
+console.log("The lowest grade is " + sortedScores[0]);
+console.log("The highest grade is " + sortedScores[sortedScores.length-1]);
 
-var gradeA = '';
-var gradeB = '';
-
-switch (sortedScores) {
-	case 101-91:
-	alert("GradeA is" + scores.length);
-	break;
-
-}	
-
-
-
-case (x < 5):
-        alert("less than five");
-        break;
-    case (x > 4 && x < 9):
-        alert("between 5 and 8");
-        break;
-for (var i = 0; i < scores.length; i++){
-	if (scores[i] > 91){
-		gradeA = console.log("There are" + i + " of students with grade A.")
-	}
-} else if (scores[i] > 81 && scores[i] < 91){
-		gradeB = console.log(i)
-} else if (scores[i] > ){
-
-}
-}
-function range(upper, lower){
-	return ;
-}
-
-var gradeA = scores.range(function(101, 91){
-console.log(gradeA.length);
-});
-
-
-console.log("The lowest grade is" + sortedScores[0]);
-console.log("The highest grade is" )
 
 
 
