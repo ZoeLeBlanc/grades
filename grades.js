ada6aa2d57156f3c90a6eb31935d33fae7b3abd6
ZoeLeBlanc <zoe.leblanc@vanderbilt.edu>
2016-07-23 09:01:44 -0500

    updates

--- grades.js
+++ grades.js
@@ -1,18 +1,26 @@
 var scores = [82, 71, 95, 55, 98, 69, 72, 78, 84, 64, 58, 87];
 
-var gradeA = '';
-var gradeB = '';
-for (var i = 0; i < scores.length; i++){
-	if (scores[i] > 91){
-		gradeA = console.log("There are" + i + " of students with grade A.")
-	}
-} else if (scores[i] > 81){
-		gradeB = console.log(i)
-} else if (scores[i] > ){
+var gradeA = scores.filter(function(score) {
+	return score > 91;
+}).length;
+console.log("There are " + gradeA + " of students with a grade 91 or higher.");
 
-}
-}
+var gradeB = scores.filter(function(score) {
+	return score > 81 ;
+}).length;
+gradeB = gradeB - gradeA; 
+console.log("There are " + gradeB + " of students with a grade between 81 and 90.");
 
+var gradeC = scores.filter(function(score) {
+	return score > 71 ;
+}).length;
 
+gradeC = gradeC - gradeB; 
+console.log("There are " + gradeC + " of students with a grade between 71 and 80.");
 
+var gradeD = scores.filter(function(score) {
+	return score > 61 ;
+}).length;
 
+gradeD = gradeD - gradeC; 
+console.log("There are " + gradeD + " of students with a grade between 61 and 70.");
